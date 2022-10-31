19b3bf162adf40f54a301b40445030b74fd3b0fe
Scattolin-Marco <scattolin.marco@bcg.com>
2022-10-31 15:34:09 +0100

    recupero del pregresso

--- src/ga_api/__init__.py
+++ src/ga_api/__init__.py
@@ -6,9 +6,9 @@ import os
 import json
 import datetime
 
-SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-KEY_FILE_LOCATION = '../../secrets/googleapi-b7fa7534555d.json'
-VIEW_ID = '274858544'
+SCOPES = ["https://www.googleapis.com/auth/analytics.readonly"]
+KEY_FILE_LOCATION = "../../secrets/googleapi-b7fa7534555d.json"
+VIEW_ID = "274858544"
 
 
 def initialize_analyticsreporting():
@@ -21,7 +21,7 @@ def initialize_analyticsreporting():
         KEY_FILE_LOCATION, SCOPES)
 
     # Build the service object.
-    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+    analytics = build("analyticsreporting", "v4", credentials=credentials)
 
     return analytics
 
@@ -31,24 +31,24 @@ def get_report(analytics, date_string):
 
   Args:
     :param analytics: An authorized Analytics Reporting API V4 service object.
-    :param date_string: date in the format 'yyyy-mm-dd', accepts also 'yesterday'
+    :param date_string: date in the format "yyyy-mm-dd", accepts also "yesterday"
   Returns:
     The Analytics Reporting API V4 response.
   """
     return analytics.reports().batchGet(
         body={
-            'reportRequests': [
+            "reportRequests": [
                 {
-                    'viewId': VIEW_ID,
-                    'dateRanges': [{'startDate': date_string, 'endDate': date_string}],
-                    'metrics': [
-                        {'expression': 'ga:sessions'},
-                        {'expression': 'ga:users'},
+                    "viewId": VIEW_ID,
+                    "dateRanges": [{"startDate": date_string, "endDate": date_string}],
+                    "metrics": [
+                        {"expression": "ga:sessions"},
+                        {"expression": "ga:users"},
                     ],
-                    'dimensions': [
-                        {'name': 'ga:date'},
-                        {'name': 'ga:source'},
-                        {'name': 'ga:channelGrouping'},
+                    "dimensions": [
+                        {"name": "ga:date"},
+                        {"name": "ga:source"},
+                        {"name": "ga:channelGrouping"},
                     ]
                 }]}).execute()
 
@@ -64,31 +64,31 @@ def make_dataframe(response):
     df_columns = []
 
     # parse dimensions
-    for report in response.get('reports', []):
-        column_header = report.get('columnHeader', {})
-        dimension_headers = column_header.get('dimensions', [])
+    for report in response.get("reports", []):
+        column_header = report.get("columnHeader", {})
+        dimension_headers = column_header.get("dimensions", [])
         for dim in dimension_headers:
             df_columns.append(dim)
 
     # parse metrics
-    for report in response.get('reports', []):
-        column_header = report.get('columnHeader', {})
-        metric_headers = column_header.get('metricHeader', [])
-        metric_header_entries = metric_headers.get('metricHeaderEntries', [])
+    for report in response.get("reports", []):
+        column_header = report.get("columnHeader", {})
+        metric_headers = column_header.get("metricHeader", [])
+        metric_header_entries = metric_headers.get("metricHeaderEntries", [])
         for metric in metric_header_entries:
-            df_columns.append(metric.get('name', ''))
+            df_columns.append(metric.get("name", ""))
 
     # empty dataframe
     df = pd.DataFrame(columns=df_columns)
 
     # iterate on data
-    for report in response.get('reports', []):
-        for row in report.get('data', {}).get('rows', []):
+    for report in response.get("reports", []):
+        for row in report.get("data", {}).get("rows", []):
             current_row = []
-            for dim in row.get('dimensions', ''):
+            for dim in row.get("dimensions", ""):
                 current_row.append(dim)
-            for metric in row.get('metrics', []):
-                for val in metric.get('values', ''):
+            for metric in row.get("metrics", []):
+                for val in metric.get("values", ""):
                     current_row.append(val)
             df.loc[len(df)] = current_row
 
@@ -127,7 +127,7 @@ def write_data(df_in):
     print("Done!")
 
 
-def main(date_string='yesterday'):
+def main(date_string="yesterday"):
     analytics = initialize_analyticsreporting()
     response = get_report(analytics, date_string=date_string)
     df = make_dataframe(response)
@@ -138,7 +138,7 @@ def main(date_string='yesterday'):
         write_data(df)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dates = pd.date_range(
         start=datetime.date(2022, 9, 6),
         end=datetime.date(2022, 10, 30)
